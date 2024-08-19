037acf5c467c040fc6cfa870b3a91a971c914177
Angel Anaya <aramirez@solvd.com>
2024-08-19 12:53:56 -0500

    add go button to go-to paginator

--- src/components/Table/components/GoToPage.tsx
+++ src/components/Table/components/GoToPage.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useContext } from "react";
+import React, { FormEvent, useCallback, useContext, useState } from "react";
 
 import { tableContext } from "../context";
 
@@ -8,30 +8,49 @@ export const GoToPage: React.FC = () => {
     totalPages,
     pageSetter: setPage,
   } = useContext(tableContext);
+  const [pageInput, setPageInput] = useState(currentPage);
 
-  const handleBlur = useCallback(
-    (evt: React.FocusEvent<HTMLInputElement>) => {
+  const handleChange = useCallback(
+    (evt: React.ChangeEvent<HTMLInputElement>) => {
       const { value } = evt.currentTarget;
       const page = Number(value) || 0;
 
       const clamped = Math.min(Math.max(page - 1, 0), totalPages - 1);
-      setPage(clamped);
+      setPageInput(clamped);
     },
-    [totalPages]
+    [totalPages, setPageInput]
+  );
+
+  const handleGo = useCallback(() => setPage(pageInput), [pageInput, setPage]);
+
+  const handleSubmit = useCallback(
+    (evt: FormEvent) => {
+      evt.preventDefault();
+      handleGo();
+    },
+    [handleGo]
   );
 
   return (
-    <div className="flex  gap-x-4">
+    <div className="flex gap-x-4 pr-4 border-r border-slate-400 dark:border-slate-800">
       <div>Go to page:</div>
       <div>
-        <input
-          type="number"
-          defaultValue={currentPage + 1}
-          className="bg-transparent border-none"
-          min={1}
-          max={totalPages}
-          onBlur={handleBlur}
-        />
+        <form onSubmit={handleSubmit}>
+          <input
+            type="number"
+            defaultValue={currentPage + 1}
+            className="bg-transparent border-none"
+            min={1}
+            max={totalPages}
+            onChange={handleChange}
+          />
+          <button
+            className="cursor-pointer rounded-full text-xs bg-slate-300 hover:bg-slate-400 active:bg-slate-300 dark:bg-slate-900 hover:dark:bg-slate-800 active:dark:bg-slate-900 border border-slate-400 dark:border-slate-800 w-8 h-8"
+            onClick={handleGo}
+          >
+            Go
+          </button>
+        </form>
       </div>
     </div>
   );
